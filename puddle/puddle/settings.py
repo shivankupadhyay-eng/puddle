292398bec1ab30c0f33a712c9498b5843335540c
shivankupadhyay-eng <shivankupadhyay51@gmail.com>
2024-02-04 23:40:01 +0530

    added a search bar

--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -134,8 +134,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-#manually added
 
+
+#manually added
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR/'media'
 
